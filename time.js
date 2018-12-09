6dac15b91b08722f5ffc137ee68622f247ba098a
Matt Holloway <hollowmatt@gmail.com>
2018-12-09 14:51:04 -0600

    Continue building function

--- time.js
+++ time.js
@@ -1,4 +1,26 @@
 // Functions for time tracker
 (function() {
 	console.log("message");
+	const timeData = [
+		{
+			"entries": {
+		  	"1:1": {
+		      "30": {
+	        	"Payan":"01/01/2018"	        
+		      },
+		      "30": {
+	        	"Nancy":"01/01/2018"	        
+		      }
+		    }      
+		  }
+		}
+	];
+
+	function buildDataSet() {
+		const output = [];
+		timeData.forEach((currentEntry, entryType) => {
+
+		});
+	}
+
 })();
